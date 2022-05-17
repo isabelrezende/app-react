0bbaeffc2f0d653b07d63d285c7765a86cbfd32a
AcrePrev <email@email.com>
2022-05-17 13:28:18 -0500

    adição de novas funções que lidam com o botão de adicionar novas taskas

--- iflix/src/components/AddTask.jsx
+++ iflix/src/components/AddTask.jsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import './AddTask.css';
 import Button from "./Button";
 
-const AddTask = () =>{
+const AddTask = ( {handleTaskAddition} ) =>{
+    const [inputData, setInputData] = useState('')
+
+    const handleInputChange = (e) =>{
+        setInputData(e.target.value);
+    }
+
+    const handleAddTaskClick = () =>{
+        handleTaskAddition(inputData)
+    }
+
+    
+
     return (
         <div className="add-task-container">
-            <input className = 'add-task-input' type="text" />
+            <input 
+                className = 'add-task-input'
+                value = {inputData}
+                type="text" 
+                onChange = {handleInputChange}
+            />
             <div className="add-task-button-container">
                 <Button>Adicionar</Button>
             </div>
